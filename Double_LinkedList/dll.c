adc808328c8a1931c96f50173a103fd5540fe619
Agnik04 <karanagnik@gmail.com>
2022-06-03 19:45:14 +0530

    Updated till date

--- Double_LinkedList/dll.c
+++ Double_LinkedList/dll.c
@@ -62,3 +62,104 @@ void traverse_prev(Node* tail)
     }
     printf("\n");
 }
+
+void delete_end(Node** tail)
+{
+    Node* store;
+
+    if(*tail == NULL)
+        return;
+
+    store = *tail;
+    (*tail)->prev->next = NULL;
+    (*tail) = (*tail)->prev;
+    free(store);
+}
+
+void delete_begin(Node** head)
+{
+    Node* store;
+
+    if(*head == NULL)
+        return;
+
+    store = *head;
+    (*head)->next->prev = NULL;
+    (*head) = (*head)->next;
+    free(store);
+}
+
+Node* search_by_pos(Node* head , int position)
+{
+    int i = 1, count = 1;
+    Node* temp = head;
+
+    if(head == NULL)
+        return NULL;
+
+    if(position <= 0)
+        return NULL;
+
+    while(head->next != NULL)
+    {
+        head = head->next;
+        count++;
+    }
+
+    head = temp;
+
+    if(position > count)
+        return NULL;
+
+    for(i=1; i<position; i++)
+    {
+        head = head->next;
+    }
+
+    return head;
+}
+
+void add_in_between_after(Node* pos , int value)
+{
+    Node* temp = create_node(value);
+
+    temp->prev = pos; 
+    temp->next = pos->next;
+    if(pos->next != NULL)
+        pos->next->prev = temp;
+    pos->next = temp;
+}
+
+void add_in_between_before(Node* pos , int value)
+{
+    Node* temp = create_node(value);
+
+    if(pos->prev != NULL)
+        pos->prev->next = temp;
+    temp->prev = pos->prev;
+    temp->next = pos;
+    pos->prev = temp;
+}
+
+void delete_in_between_after(Node* pos)
+{
+    Node* temp;
+    if(pos->next == NULL)
+        return;
+    temp = pos->next;
+    pos->next = temp->next;
+    temp->next->prev = pos;
+    free(temp);
+}
+
+void delete_in_between_before(Node* pos)
+{
+    Node* temp;
+
+    if(pos->prev == NULL)
+        return;
+    temp = pos->prev;
+    pos->prev = temp->prev;
+    temp->prev->next = pos;
+    free(temp);
+}
--- Double_LinkedList/dll_m.c
+++ Double_LinkedList/dll_m.c
@@ -2,9 +2,9 @@
 
 int main()
 {
-    Node* head = NULL, *tail = NULL;
+    Node* head = NULL, *tail = NULL, *temp;
 
-    int ch, value;
+    int i, ch, value, position;
 
     while(1)
     {
@@ -13,7 +13,14 @@ int main()
         printf("2: To add node at the begin.\n");
         printf("3: To print the list.\n");
         printf("4: To print the list in reverse order.\n");
-        printf("5: To EXIT.\n\n");
+        printf("5: To delete a node from end.\n");
+        printf("6: To delete a node from beginning.\n");
+        printf("7: To search a value by position.\n");
+        printf("8: To add a node after a position in the list.\n");
+        printf("9: To add a node before a position in the list.\n");
+        printf("10: To delete a node after a position in the list.\n");
+        printf("11: To delete a node before a position in the list.\n");
+        printf("12: To EXIT.\n\n");
 
         printf("Enter your choice :\n");
         scanf("%d",&ch);
@@ -37,6 +44,67 @@ int main()
                 traverse_prev(tail);
                 break;
             case 5:
+                delete_end(&tail);
+                break;
+            case 6:
+                delete_begin(&head);
+                break;
+            case 7:
+                printf("Enter the position at which the value to be searched :");
+                scanf("%d",&position);
+                temp = search_by_pos(head , position);
+                if(temp == NULL)
+                    printf("INVALID POSITION\n");
+                else
+                    printf("%d ",temp->data);
+                break;
+            case 8:
+                printf("Enter the position after which a node will be added :");
+                scanf("%d",&position);
+
+                printf("Enter the value :");
+                scanf("%d",&value);
+
+                temp = search_by_pos(head, position);
+                if(temp == NULL)
+                    printf("INVALID POSITION\n");
+                else
+                    add_in_between_after(temp , value);
+                break;
+            case 9:
+                printf("Enter the position before which a node will be added :");
+                scanf("%d",&position);
+
+                printf("Enter the value :");
+                scanf("%d",&value);
+
+                temp = search_by_pos(head, position);
+                if(temp == NULL)
+                    printf("INVALID POSITION\n");
+                else
+                    add_in_between_before(temp, value);
+                break;
+            case 10:
+                printf("Enter the position after which a node will be deleted :");
+                scanf("%d",&position);
+
+                temp = search_by_pos(head, position);
+                if(temp == NULL)
+                    printf("INVALID POSITION\n");
+                else
+                    delete_in_between_after(temp);
+                break;
+            case 11:
+                printf("Enter the position before which a node will be deleted :");
+                scanf("%d",&position);
+
+                temp = search_by_pos(head, position);
+                if(temp == NULL)
+                    printf("INVALID POSITION\n");
+                else
+                    delete_in_between_before(temp);
+                break;
+            case 12:
                 return 0;
             default:
                 printf("Enter the correct choice.\n");
